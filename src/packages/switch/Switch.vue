917a64f668a3a885fca6531034351e0a54e838d2
zhenhui.cao <zhenhui.cao@ikang.com>
2019-11-04 18:30:57 +0800

    加入switch

---
+++ src/packages/switch/Switch.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="switch">
+    Switch
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Switch'
+}
+</script>
+
+<style lang="scss">
+.switch {
+  color: red;
+}
+</style>
